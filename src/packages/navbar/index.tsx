0e66b0cfe820654e83bd20c8361801af0e32653b
taivu <tai180597@gmail.com>
2023-02-27 23:26:01 +0700

    fix: update img tag

--- src/packages/navbar/index.tsx
+++ src/packages/navbar/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import { useAuth } from '../common/hooks/useAuth';
@@ -12,7 +11,7 @@ const Navbar = () => {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 container'>
         <div className='flex items-center justify-between h-16'>
           <div className='flex-shrink-0'>
-            <Image className='h-8 w-auto' src='https://jitera.com/images/logo.svg' alt='Logo' />
+            <img className='h-8 w-auto' src='https://jitera.com/images/logo.svg' alt='Logo' />
           </div>
           <div className='hidden md:block'>
             <div className='ml-4 flex items-center md:ml-6'>
